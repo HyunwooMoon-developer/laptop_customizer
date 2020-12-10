35e2b9db176a61bf5ca93615a0161078810cd9e8
Hyunwoo Moon <mhw9163@gmail.com>
2020-12-09 20:39:42 -0800

    submit

--- src/Cart/CartTotal.js
+++ src/Cart/CartTotal.js
@@ -1,11 +1,9 @@
 import React from 'react';
+import USCurrencyFormat from '../USCurrencyFormat';
 
 class CartTotal extends React.Component{
     render(){
-      const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'})
-
+     
       const total = Object.keys(this.props.selected).reduce(
         (acc, curr) => acc + this.props.selected[curr].cost,
         0
